2e850779c77bbb69332bde77a4cb116437ae46c0
Spedon <realsped0n@outlook.com>
2023-09-24 20:35:40 +0800

    feat(tb): add tb_hs_ad_da module

---
+++ ada.srcs/testbench/tb_hs_ad_da.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/24/2023 08:32:12 PM
+// Design Name: 
+// Module Name: tb_hs_ad_da
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_hs_ad_da(
+
+    );
+endmodule
