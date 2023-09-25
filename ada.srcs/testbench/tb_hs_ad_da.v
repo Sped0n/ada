e7e13f7843ca7fc4ff057005a46d16051f639168
Spedon <realsped0n@outlook.com>
2023-09-26 01:03:16 +0800

    test: update hs_ad_da testbench

--- ada.srcs/testbench/tb_hs_ad_da.v
+++ ada.srcs/testbench/tb_hs_ad_da.v
@@ -23,21 +23,36 @@
 module tb_hs_ad_da ();
   // parameter define
   parameter CLK_PERIOD = 20;  // clock period in ns
+  parameter DEBOUNCE_CNT_MAX = 20'd1000;  // debounce for 1000 * 20ns(1s/50MHz) = 20000ns / 20us
 
   // reg define
   reg sys_clk;  // system clock
   reg sys_rst_n;  // system reset active low
+  reg [3:0] keys;  // keys
 
   // signal init
   initial begin
-    sys_clk   = 1'b0;
+    keys = 4'b1111;
+    sys_clk = 1'b0;
     sys_rst_n = 1'b0;
     #200 sys_rst_n = 1'b1;
+    // key signal change
+    #32000 keys = 4'b0111;
+    #2000 keys = 4'b1111;
+    #5000 keys = 4'b0111;
+    #4000 keys = 4'b1111;
+    #2000 keys = 4'b0111;
+    #30000 keys = 4'b1111;
+    #5000 keys = 4'b0111;
+    #4000 keys = 4'b1111;
+    #30000 keys = 4'b0111;
   end
 
   // clock generator
   always #(CLK_PERIOD / 2) sys_clk = ~sys_clk;
-  hs_ad_da hs_ad_da_0 (
+  hs_ad_da #(
+      .DEBOUNCE_CNT_MAX(DEBOUNCE_CNT_MAX)
+  ) hs_ad_da_0 (
       .sys_clk  (sys_clk),    // system clock
       .sys_rst_n(sys_rst_n),  // system reset active low
       // dac interface
@@ -46,6 +61,10 @@ module tb_hs_ad_da ();
       // adc interface
       .ad_data  (),           // adc input data
       .ad_otr   (),           // 0: adc data is valid, 1: adc data is invalid (over range)
-      .ad_clk   ()            // adc clock
+      .ad_clk   (),           // adc clock
+      // buzzer
+      .buzzer   (),           // buzzer (beep when adc data is over range)
+      // keys
+      .keys     (keys)        // keys
   );
 endmodule
