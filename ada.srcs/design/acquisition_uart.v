d66faeec569e47f0908acfcb48c029f7ec3c000b
Spedon <realsped0n@outlook.com>
2023-09-29 01:30:09 +0800

    feat(design): add acquisition_uart module

---
+++ ada.srcs/design/acquisition_uart.v
@@ -0,0 +1,84 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: spedon wen
+// 
+// Create Date: 09/29/2023 01:03:43 AM
+// Design Name: 
+// Module Name: acquisition_uart
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module acquisition_uart (
+    input        clk_50m,
+    input        clk_25m,
+    input        sys_rst_n,
+    input        trigger,
+    input  [7:0] ad_data,
+    output       uart_txd
+);
+  // wire define
+  wire wr_rst_busy;
+  wire rd_rst_busy;
+  wire wr_en;
+  wire rd_en;
+  wire wr_data;
+  wire rd_data;
+  wire almost_full;
+  wire almost_empty;
+  wire full;
+
+  // main code
+
+  // fifo generator
+  async_fifo_8b async_fifo_8b_0 (
+      .rst         (~sys_rst_n),
+      .wr_clk      (clk_25m),
+      .rd_clk      (clk_50m),
+      .wr_en       (wr_en),
+      .rd_en       (rd_en),
+      .din         (wr_data),
+      .dout        (rd_data),
+      .almost_full (almost_full),
+      .almost_empty(almost_empty),
+      .full        (full),
+      .empty       (),
+      .wr_rst_busy (wr_rst_busy),
+      .rd_rst_busy (rd_rst_busy)
+  );
+
+  // fifo write
+  acquisition_fifo_wr acquisition_fifo_wr_0 (
+      .wr_clk     (clk_25m),
+      .rst_n      (sys_rst_n),
+      .wr_rst_busy(wr_rst_busy),
+      .trigger    (trigger),
+      .almost_full(almost_full),
+      .wr_en      (wr_en),
+      .wr_data    (wr_data),
+      .ad_data    (ad_data)
+  );
+
+  // fifo read
+  acquisition_fifo_rd_uart acquisition_fifo_rd_uart_0 (
+      .rd_clk      (clk_50m),
+      .rst_n       (sys_rst_n),
+      .rd_rst_busy (rd_rst_busy),
+      .rd_data     (rd_data),
+      .full        (full),
+      .almost_empty(almost_empty),
+      .rd_en       (rd_en),
+      .uart_txd    (uart_txd)
+  );
+endmodule
