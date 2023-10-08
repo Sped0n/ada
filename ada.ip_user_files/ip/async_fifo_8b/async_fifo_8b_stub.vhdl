5e76fbc19077430bb0ba97eee95b192bd60a9352
Spedon <realsped0n@outlook.com>
2023-10-09 05:37:29 +0800

    feat(async_fifo_8b): add stub files for async_fifo_8b IP
    Add stub files for the async_fifo_8b IP. These stub files are used to infer a black box for the IP during synthesis. The Verilog stub file `async_fifo_8b_stub.v` and the VHDL stub file `async_fifo_8b_stub.vhdl` declare the top-level module interface of the async_fifo_8b IP. These stub files prevent IO buffer insertion and provide synthesis directives for Synopsys Synplify support.

---
+++ ada.ip_user_files/ip/async_fifo_8b/async_fifo_8b_stub.vhdl
@@ -0,0 +1,42 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
+-- Date        : Mon Oct  9 05:01:06 2023
+-- Host        : Zen running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/spedon/Documents/eeworks/FPGA/ada/ada.srcs/ips/async_fifo_8b/async_fifo_8b_stub.vhdl
+-- Design      : async_fifo_8b
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tfgg484-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity async_fifo_8b is
+  Port ( 
+    rst : in STD_LOGIC;
+    wr_clk : in STD_LOGIC;
+    rd_clk : in STD_LOGIC;
+    din : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    wr_en : in STD_LOGIC;
+    rd_en : in STD_LOGIC;
+    dout : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    full : out STD_LOGIC;
+    almost_full : out STD_LOGIC;
+    empty : out STD_LOGIC;
+    almost_empty : out STD_LOGIC;
+    wr_rst_busy : out STD_LOGIC;
+    rd_rst_busy : out STD_LOGIC
+  );
+
+end async_fifo_8b;
+
+architecture stub of async_fifo_8b is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "rst,wr_clk,rd_clk,din[7:0],wr_en,rd_en,dout[7:0],full,almost_full,empty,almost_empty,wr_rst_busy,rd_rst_busy";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "fifo_generator_v13_2_7,Vivado 2022.2";
+begin
+end;
